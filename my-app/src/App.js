d63226fdd8a72699fc7177cdc03f511149f66c1c
Romero <gandresrp@gmail.com>
2021-03-02 23:16:44 -0300

    JSX-Templates - Ciclos

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,8 +18,36 @@ const Saludo = () => {
       {
         idioma === "es" && <p><SaludarEnIdiomas idioma={idioma}/> { `${nombreES}` } </p>
       }
+      {
+        idioma === "en" ? <p>Hello</p> : <p>Hola</p>
+      }
     </div>
   )
+}
+
+const nombres = [
+  'Gustavo',
+  'Marisol',
+  'Estefania',
+]
+
+/*
+//Ciclo For
+function getNombres(){
+  const elementosLista = [];
+  for(let i=0; i < nombres.length; i++){
+    elementosLista.push(<li key={nombres[i].toString()}>{nombres[i]}</li>);
+  }
+  return elementosLista;
+}
+*/
+
+//Usando Map
+const Nombres = () => {
+  //return <ul>{getNombres()}</ul>
+  return <ul>{
+      nombres.map((nombre, index) => <li key={index}>{nombre}</li>)
+    }</ul>
 }
 
 function App() {
@@ -28,6 +56,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Saludo/>
+        <Nombres/>
       </header>
     </div>
   );
