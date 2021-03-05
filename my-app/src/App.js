4bdcda45277d882b162c53dda5303fc8cfe45d32
Romero <gandresrp@gmail.com>
2021-03-05 17:23:36 -0300

    Formularios - Manipulando el DOM directamente

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,13 +6,20 @@ import './App.css';
 //import ExampleLifecycle from './curso/componentes/Ciclo-de-vida-de-un-componente'
 //import {Eventos} from './curso/componentes/Eventos'
 import Formulario from './curso/formularios/leyendo-el-valor-de-los-controles'
+import { useState } from 'react';
 
 function App() {
+  const [toggleForm, setToggleForm] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          <Formulario/>
+        <button onClick={() => setToggleForm(!toggleForm)}>ShowForm</button>
+        {
+          toggleForm  && <Formulario showed={toggleForm}/> 
+        }
+          
       </header>
     </div>
   );
--- my-app/src/curso/formularios/leyendo-el-valor-de-los-controles.js
+++ my-app/src/curso/formularios/leyendo-el-valor-de-los-controles.js
@@ -1,8 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
-const Formulario = () => {
+const Formulario = ({showed}) => {
   const [title, setTitle] =  useState("");
   const [body, setBody] = useState("");
+
+  //REferencia al nodo del DOM que queremos manipular
+  const inputRef = useRef();
+
+  useEffect(() => {
+    //Actualzia el DOM
+    if(inputRef){
+      console.log(inputRef);
+      //use ref retorna current, que corresponde la objeto o nodo que queremos manipular
+      inputRef.current.focus();
+    }
+    return () => {
+      console.log("no visible")
+    }
+  }, [showed])
   
   //Font: https://jsonplaceholder.typicode.com/guide/
   const sendForm = (ev) => {
@@ -28,7 +43,7 @@ const Formulario = () => {
     <form onSubmit={(ev) => sendForm(ev)}>
       <div>
         <label htmlFor="title">Título</label>
-        <input type="text" id="title" value={title} onChange={(ev)=>setTitle(ev.target.value)}/>
+        <input type="text" id="title" value={title} onChange={(ev)=>setTitle(ev.target.value)} ref={inputRef}/>
       </div>
       <div>
         <label htmlFor="body">Publicación</label>
