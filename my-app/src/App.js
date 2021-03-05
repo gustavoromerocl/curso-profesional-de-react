efc1a1b0472650567a432af7b3430e003c886daa
Romero <gandresrp@gmail.com>
2021-03-05 20:14:22 -0300

    Code splitting - React lazy y Suspense

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,22 +5,27 @@ import './App.css';
 //import ExampleState from './curso/componentes/State'
 //import ExampleLifecycle from './curso/componentes/Ciclo-de-vida-de-un-componente'
 //import {Eventos} from './curso/componentes/Eventos'
-import Surprise from './curso/code-splitting/Surprise'
-import { useState } from 'react';
+//import Surprise from './curso/code-splitting/Surprise'
+import React, { Suspense, useState } from 'react';
 
 function App() {
   const [showSurprise, setShowSurprise] = useState(false);
 
+  //React Lazy suspende la descarga de un componente hasta que este es llamado, el componente debe estar anidado en un Suspense
+  const Surprise = React.lazy(() => import('./curso/code-splitting/Surprise'))
+
   return (
     <div className="App">
+      <Suspense fallback={<p>Cargando...x|</p>}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <button onClick={() => setShowSurprise(!showSurprise)}>Show surprise</button>
         {
-          showSurprise  && <Surprise/> 
+          showSurprise  &&  <Surprise/> 
         }
           
       </header>
+      </Suspense>
     </div>
   );
 }
